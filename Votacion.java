6b613461227a914aab54279411759842ccd9bba5
Eduardo Cabrera Montero <isc.eduardo.came@gmail.com>
2024-06-14 18:08:29 -0600

    ignorando archivos .class y formateado del codigo del archivo Votacion.java

--- Votacion.java
+++ Votacion.java
@@ -1,28 +1,44 @@
-package ejemplo;
-
 import java.util.Scanner;
 import java.util.Random;
+/*
+ * crear un programa que dado el total de votos deseados de una eleccion, reparta de forma
+ * aleatoria los votos, dependiendo de los posibles patidos a elejir
+ * 
+ */
 public class Votacion{
-    public static void main(String args[]){
-        Scanner s= new Scanner(System.in); 
-        int partido, votos, elecciones, i=0, votosResiduales;
+    /*
+     * se crea la funcion votacion con los parametros votos, elecciones
+     * se crean las variables enteras i , votos residuales, partido y el objecto random
+     * se implementa la estrucura while para repetir hasta el decremento de las cantidades de las elecciones llegue a 0
+     * se obtiene la cantidad de votos aleatorios para cada partido -> (numero_aleatorio_decimal * votos)/2
+     * se obtiene los votos residuales  -> votos - votos_aleatorios_partido
+     * se imprime el partido, los votos para ese patido y los residuales
+     * se realiza el trazpaso de valores [votos_residuales a votos] para que por medio del decremento de la cantidad de elecciones se vuelva a ciclo while
+     */ 
+    public static void votacion(int votos, int elecciones ){
+        int i=0, votosResiduales, partido;
         Random r=new Random();
-        System.out.println("votos");
-        votos=s.nextInt();
-        System.out.println("elecciones");
-        elecciones=s.nextInt();
-        while (elecciones>0) {
+         while (elecciones>0) {
             i++;
-            String aux=String.valueOf(r.nextDouble()*votos);
-            //System.out.println(aux.split("\\.")[0]);
-            
-            partido=Integer.parseInt(String.valueOf(r.nextDouble()*votos).split("\\.")[0]);
+            partido=Integer.parseInt(String.valueOf((r.nextDouble()*votos)/2).split("\\.")[0]);
             votosResiduales=votos-partido;
-            System.out.println("partido "+ i+ " = " + partido);
+ 
+            System.out.println("partido "+ i + " = " + partido);
             System.out.println("residuales " + votosResiduales);
+            System.out.println("_=====================_");
             
             votos=votosResiduales;
             elecciones--;
         }
+    }
+
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in); 
+        System.out.println("votos");
+        int votos=s.nextInt();
+        System.out.println("elecciones");
+        int elecciones=s.nextInt();
+        votacion(votos, elecciones);
+
     }
 }
